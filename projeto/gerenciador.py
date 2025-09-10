539425ebe9c84ab6e51048de692821171149a4d3
Ana Livia <anaa.moraes.9@gmail.com>
2025-09-10 15:21:59 -0300

    while e iniciando o projeto

---
+++ projeto/gerenciador.py
@@ -0,0 +1,15 @@
+while True:
+  print("\nMenu do Gerenciador de Lista de Tarefas:") 
+  print("\n1. Adicionar uma tarefa")
+  print("2. Ver tarefa")
+  print("3. Atualizar tarefa")
+  print("4. Completar tarefa")
+  print("5. Deletar tarefas completadas")
+  print("6. Sair")
+
+  escolha = input("Digite a sua escolha: ")
+
+  if escolha == "6":
+    break
+  
+  print("Programa finalizado.")
---
+++ while.py
@@ -0,0 +1,12 @@
+#While: é um tipo de loop utilizado para repetir um bloco de código enquanto uma condição for verdadeira. 
+print("Exemplo de while:")
+contador = 0
+while contador <5:
+  print("Contagem", contador)
+  contador += 1
+  if contador == 4:
+    break
+print("Programa finalizado")
+
+#Break força a saída do loop 
+
