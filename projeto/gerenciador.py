7c4f96573dd6599eed2a77aed11cb91f782dfefc
Ana Livia <anaa.moraes.9@gmail.com>
2025-09-10 15:49:27 -0300

    adicionando funções ao projeto

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -1,3 +1,13 @@
+def adicionar_tarefa(tarefas, nome_tarefa):
+
+  #Tarefas: nome da tarefa 
+  #Completada: indica se essa tarefa foi completada ou não
+  tarefa = {"tarefa": nome_tarefa, "completada": False}
+  tarefas.append(tarefa)
+  print(f"A tarefa {nome_tarefa} foi adicionada com sucesso!")
+  return
+
+tarefas = []
 while True:
   print("\nMenu do Gerenciador de Lista de Tarefas:") 
   print("\n1. Adicionar uma tarefa")
@@ -9,7 +19,11 @@ while True:
 
   escolha = input("Digite a sua escolha: ")
 
-  if escolha == "6":
+  if escolha == "1":
+    nome_tarefa = input("Digite qual tarefa deseja adicionar: ")
+    adicionar_tarefa(tarefas, nome_tarefa)
+
+  elif escolha == "6":
     break
   
   print("Programa finalizado.")
